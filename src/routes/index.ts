08a73d7dd61b6c897d8b79f65f7d01a5b69eaeb2
Peem <peemppk5538@gmail.com>
2018-08-03 13:07:13 +0700

    แก้ไขเทพรัตน์

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2367,8 +2367,7 @@ router.get('/report/getporder/DebaratanaNakhonratchasima/', wrap(async (req, res
       if (v.standard_cost <= v.unit_price) {
         v.standard_cost = v.unit_price
       }
-      v.total_price = v.sub_total
-      total = v.totalprice;
+      total = v.sub_total;
       v.total_price = model.comma(v.total_price);
       v.qty = model.commaQty(v.qty);
       v.unit_price = model.comma(v.unit_price);
